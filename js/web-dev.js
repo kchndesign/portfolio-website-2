ac3bd9a6ba5a43eff9f70a4b8076790402c6985e
Work <kevin@kchn.com.au>
2022-03-16 15:21:42 +1100

    added contact to web-dev page and light mode

---
+++ js/web-dev.js
@@ -0,0 +1,8 @@
+// when window loads, trigger tooltip to show user that there is a light mode option
+
+window.onload = () => {
+    const element = document.querySelector('.toggle__wrapper');
+
+    console.log('triggering mouseenters');
+    $('.toggle__wrapper');
+};
