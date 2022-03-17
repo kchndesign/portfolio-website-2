a7a6b4a8876f4d67b3476945c2a416ff3737011a
Work <kevin@kchn.com.au>
2022-03-17 12:15:26 +1100

    added scss file for graphics design page

--- js/web-dev.js
+++ js/web-dev.js
@@ -1,8 +1,7 @@
 // when window loads, trigger tooltip to show user that there is a light mode option
 
-window.onload = () => {
-    const element = document.querySelector('.toggle__wrapper');
+// window.onload = () => {
+//     const element = document.querySelector('.toggle__wrapper');
 
-    console.log('triggering mouseenters');
-    $('.toggle__wrapper');
-};
+//     console.log('triggering hover');
+// };
